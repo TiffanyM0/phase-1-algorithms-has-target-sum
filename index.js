79be43967f2fa250d4646a3927ca83062b3de81b
TIFFANY MAINA <tiffmaina@gmail.com>
2023-10-27 17:11:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    let result =  target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === result) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  for 2 iterations = O(n^2);
 */
 
 /* 
   Add your pseudocode here
+  iterate over array; taking first element
+    find the difference of target - first element;
+    loops over array, check for element === difference;
+      return true or false;
 */
 
 /*
   Add written explanation of your solution here
+  iterate over the array; 
+    to get the no that when added to array[i] = target; get the difference;
+    loop through array, check if any element match the difference;
+    return true;
+  if none; exit loop, return false
 */
 
 // You can run `node index.js` to view these console logs
